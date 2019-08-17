77bbf02aaafdc46a64adbe3f2bd433b669984ac7
“meagancojocar” <meagan.cojocar@gmail.com>
2019-08-17 11:31:01 +0800

    working through ex 2

--- week-2/exercise-2.py
+++ week-2/exercise-2.py
@@ -1,6 +1,5 @@
-# TASK 1
-# given a 5x5 grid, add the last column and row, then flip the card at the
-# coordinate specified by the user
+# # TASK 1
+# --- Instruction: given a 5x5 grid, add the last column and row, then flip the card at the coordinate specified by the user
 
 five_by_five_grid = [
     ['X','0','X','X','X'],
@@ -10,38 +9,35 @@ five_by_five_grid = [
     ['X','0','0','X','X'],
 ]
 
-# first step is to add colum 6 and row 6
+# --- Instruction: first step is to add colum 6 and row 6
 
+x_counter = 0
 for row in five_by_five_grid:
-    x_counter = 0 
     for char in row:
-        if char == "X":
+        if char == 'X':
             x_counter +=1
+    if x_counter % 2 == 0:
+        row.append('0')
+    else:
+        row.append('X')
 
 
-# for row in five_by_five_grid:
+five_by_five_grid.append(['0','X','0','X','0','X'])
+    
+# --- Instruction: output the grid to the user
+print(five_by_five_grid)
 
 
-# five_by_five_grid.append('X')
+# --- Instruction: ask the user for the coordinate of the card to flip e.g. input could be: (0,2)
+message = input('What coordinate of card would you like to flip: ')
 
-# output the grid to the user
-# for card in five_by_five_grid:
-#     print(card)
+message.strip('()')
 
-# # ask the user for the coordinate of the card to flip
-# message = input('What coordinate of card would you like to flip: ')
 
-# # message..strip('()')
-
-# print(five_by_five_grid)
-
-# e.g. input could be: (0,2)
-
-# output the grid with the flipped card
+# --- Instruction: output the grid with the flipped card
 
 ## TASK 2
 
-# given a six by six grid, work out what card was flipped
-
+# --- Instruction: given a six by six grid, work out what card was flipped
 # your program should output the coordinate of the flipped card
 # in this case it would be: (x,y)
