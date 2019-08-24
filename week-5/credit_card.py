81616ee669bdc1ee794b9a72eadf95c9cd07fe93
“meagancojocar” <meagan.cojocar@gmail.com>
2019-08-24 12:22:09 +0800

    refactoring

--- week-5/credit_card.py
+++ week-5/credit_card.py
@@ -6,21 +6,21 @@ def calculate_credit_card_check_digit(card_number):
         if digit % 2 == 0:
             digit_times_two = int(list_of_digits[digit]) * 2
             if digit_times_two > 9:
-                sum += (digit_times_two - 9)
+                sum += digit_times_two - 9
             else:
                 sum += digit_times_two
         else:
-            sum += digit_times_two
+            sum += int(list_of_digits[digit])
     sum = str(sum * 9)
     return sum[(len(sum) - 1)]
 
 ## --- Instruction: Should return 5 if card_number = 542418027979176    
-print(calculate_credit_card_check_digit('601100099301097'))    
+print(calculate_credit_card_check_digit('542418027979176'))    
 
 ## --- Instruction: Check the check digit number to see if a value credit card number 
 def check_credit_card_check_digit(card_number):
-    check_digit = card_number[(len(card_number) - 1)]
-    correct_check_digit = calculate_credit_card_check_digit(card_number[:(len(card_number) - 1)])
+    check_digit = int(card_number[(len(card_number) - 1)])
+    correct_check_digit = int(calculate_credit_card_check_digit(card_number[:(len(card_number) - 1)]))
     if check_digit == 0 and correct_check_digit == 10:
         return ('This credit card number is valid.')
     elif check_digit != 0 and correct_check_digit == check_digit:
@@ -28,4 +28,4 @@ def check_credit_card_check_digit(card_number):
     else:
         return ('This credit card number is invalid.')
 
-print(check_credit_card_check_digit('601100099301097'))
+print(check_credit_card_check_digit('5424180279791765'))
