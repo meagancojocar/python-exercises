eb1fb25887fc5b73cd1ee59e12dcfd7970dab39f
“meagancojocar” <meagan.cojocar@gmail.com>
2019-08-24 12:00:08 +0800

    refactored

--- week-5/credit_card.py
+++ week-5/credit_card.py
@@ -1,30 +1,31 @@
 ## --- Instruction: calculate check digit 
 def calculate_credit_card_check_digit(card_number):
     list_of_digits = list(card_number[::-1])
-    doubled = 0
-    total = 0
-    for digit in list(range(0,len(list_of_digits))):
+    sum = 0
+    for digit in range(0,len(list_of_digits)):
         if digit % 2 == 0:
             digit_times_two = int(list_of_digits[digit]) * 2
             if digit_times_two > 9:
-                total += (digit_times_two - 9)
+                sum += (digit_times_two - 9)
             else:
-                total += digit_times_two
+                sum += digit_times_two
         else:
-            total += digit_times_two
-    total = str(total * 9)
-    return str(total[(len(total) - 1)])
-    
-# print(calculate_credit_card_check_digit('401200003333002'))    
+            sum += digit_times_two
+    sum = str(sum * 9)
+    return sum[(len(sum) - 1)]
+
+## --- Instruction: Should return 5 if card_number = 542418027979176    
+print(calculate_credit_card_check_digit('601100099301097'))    
 
 ## --- Instruction: Check the check digit number to see if a value credit card number 
 def check_credit_card_check_digit(card_number):
-    check_digit = int(card_number[(len(card_number) - 1)])
-    correct_check_digit = int(calculate_credit_card_check_digit(card_number[:(len(card_number) - 1)]))
+    check_digit = card_number[(len(card_number) - 1)]
+    correct_check_digit = calculate_credit_card_check_digit(card_number[:(len(card_number) - 1)])
     if check_digit == 0 and correct_check_digit == 10:
         return ('This credit card number is valid.')
     elif check_digit != 0 and correct_check_digit == check_digit:
-        return ('This credit card number is valid')
+        return ('This credit card number is valid.')
     else:
         return ('This credit card number is invalid.')
 
+print(check_credit_card_check_digit('601100099301097'))
